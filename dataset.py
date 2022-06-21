e343c3bad5663b0a20fa3dd44da0560163ce264f
unknown <hidir.yesiltepe.ai@gmail.com>
2022-06-21 19:17:54 +0300

    updated

--- dataset.py
+++ dataset.py
@@ -12,7 +12,7 @@ def get_data_loader(datasetname, root, batch_size):
                               transforms.Resize((128,128)),
                               # transforms.CenterCrop(64),
                               transforms.ToTensor(),
-                              # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                              transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                               ])
                     )
       dataloader = torch.utils.data.DataLoader(
@@ -41,6 +41,23 @@ def get_data_loader(datasetname, root, batch_size):
                               pin_memory = True
       )
 
+  elif datasetname == 'Celeb-A':
+    dataset = torchvision.datasets.CelebA(
+                    root = root,
+                    transform = transforms.Compose([
+                              transforms.Resize((128,128)),
+                              transforms.ToTensor(),
+                              transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                              ])
+                    )
+    dataloader = torch.utils.data.DataLoader(
+                              dataset,
+                              batch_size = batch_size,
+                              num_workers = 2,
+                              pin_memory = True,
+                              shuffle=True
+      )
+            
   else:
     raise ValueError(f'No dataset named {datasetname}!')
   
--- download.sh
+++ download.sh
@@ -3,3 +3,6 @@ git clone https://github.com/fyu/lsun.git
 python3 lsun/download.py -c church_outdoor
 rm -rf lsun
 unzip church_outdoor_train_lmdb.zip
+
+# Celeb-A Dataset 
+https://drive.google.com/uc?id=0B7EVK8r0v71pZjFTYXZWM3FlRnM&export=download
