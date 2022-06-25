561e28781762b2ab9aaff890b538aaa24ccb0758
finch <m.alpayozkan@gmail.com>
2022-06-25 15:26:31 +0300

    deleted unnec files

--- download.sh
+++
@@ -1,8 +0,0 @@
-# LSUN Church Dataset
-git clone https://github.com/fyu/lsun.git
-python3 lsun/download.py -c church_outdoor
-rm -rf lsun
-unzip church_outdoor_train_lmdb.zip
-
-# Celeb-A Dataset 
-gdown https://drive.google.com/uc?id=0B7EVK8r0v71pZjFTYXZWM3FlRnM&export=download
